6539b82d497de01974b5ec41d131d24b86ad12e4
PaulTest12 <mckfc30@gmail.com>
2025-08-31 18:58:21 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,2 +1,42 @@
-[web]
-vm1 ansible_host=127.0.0.1 ansible_port=2222 ansible_user=root ansible_ssh_pass=root ansible_connection=ssh ansible_ssh_common_args='-o StrictHostKeyChecking=no'
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.6"
+    }
+  }
+}
+
+provider "docker" {}
+
+# 1. Pobranie obrazu Dockera
+resource "docker_image" "sshd" {
+  name = "rastasheep/ubuntu-sshd:latest"
+}
+
+# 2. Tworzymy kontener (nasza VM)
+resource "docker_container" "vm" {
+  name  = "vm1"
+  image = docker_image.sshd.name
+
+  # Port SSH
+  ports {
+    internal = 22
+    external = 2222
+  }
+
+  # Port HTTP (nginx)
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
+
+# 3. Generowanie inventory dla Ansible
+resource "local_file" "ansible_inventory" {
+  content = templatefile("${path.module}/inventory.tpl", {
+    ip   = "127.0.0.1"
+    port = 2222
+  })
+
+  filename = "${path.module}/../ansib
