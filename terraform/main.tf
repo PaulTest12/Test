6dea45870837bd4d09936e17d10c3e7516679aa4
PaulTest12 <mckfc30@gmail.com>
2025-08-31 19:04:56 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,42 +1,20 @@
-terraform {
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 3.6"
-    }
-  }
-}
+.PHONY: all init up provision destroy clean down
 
-provider "docker" {}
+all: up provision
 
-# 1. Pobranie obrazu Dockera
-resource "docker_image" "sshd" {
-  name = "rastasheep/ubuntu-sshd:latest"
-}
+init:
+	cd terraform && terraform init
 
-# 2. Tworzymy kontener (nasza VM)
-resource "docker_container" "vm" {
-  name  = "vm1"
-  image = docker_image.sshd.name
+up: init
+	cd terraform && terraform apply -auto-approve
 
-  # Port SSH
-  ports {
-    internal = 22
-    external = 2222
-  }
+provision:
+	ansible-playbook -i ansible/hosts ansible/site.yml
 
-  # Port HTTP (nginx)
-  ports {
-    internal = 80
-    external = 8080
-  }
-}
+destroy:
+	cd terraform && terraform destroy -auto-approve
 
-# 3. Generowanie inventory dla Ansible
-resource "local_file" "ansible_inventory" {
-  content = templatefile("${path.module}/inventory.tpl", {
-    ip   = "127.0.0.1"
-    port = 2222
-  })
+clean:
+	rm -f ansible/hosts
 
-  filename = "${path.module}/../ansib
+down: destroy clean
